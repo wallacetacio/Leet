6a3a5531a42c165e775fbb45fdc986e2ca9644f9
Wallace Tácio <wallacett.dev@gmail.com>
2021-08-26 21:18:46 -0300

    Adiciona resolução do exercício Leet ETEC 2021

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Leet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        string textoDigitado;
+        Console.Write("Digite um texto e eu o traduzirei para o Leet: ");
+           textoDigitado = Console.ReadLine()
+           .Replace("a","4")
+           .Replace("A","4")
+           .Replace("e","3")
+           .Replace("E","3")
+           .Replace("i","1")
+           .Replace("I","1")
+           .Replace("l","1")
+           .Replace("L","1")
+           .Replace("o","0")
+           .Replace("O","0")
+           .Replace("t","7")
+           .Replace("T","7")
+           .Replace("s","5")
+           .Replace("S","5");
+           Console.Write("TEXTO TRADUZIDO: ");
+           Console.WriteLine(textoDigitado);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Leet
 Exercício Leet ETEC 2021
+
+**Enunciado**:
+
+Faça um tradutor simples para a linguagem [Leet Speak](https://pt.wikipedia.org/wiki/Leet).
+
+Para isso, faça as seguintes substituições:
+
+Entrada | Saída
+-- | --
+ `A` ou `a` | `4`
+ `E` ou `e` | `3`
+ `I` ou `i` ou `L` ou `l`| `1`
+ `O` ou `o` | `0`
+ `T` ou `t` | `7`
+ `S` ou `s` | `5`
+
+Exemplo:
+
+```
+Texto a ser traduzido...: Hackers usavam leet antes de virar modinha.
+TRADUZIDO: H4ck3r5 u54v4m 1337 4n735 d3 v1r4r m0d1nh4.
+```
